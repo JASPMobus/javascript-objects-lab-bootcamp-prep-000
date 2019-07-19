22c281b902b6f1ce03ba77dd8a0110ce6bb19114
James Mobus <mobusj@live.com>
2019-07-19 19:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = (key: value)
-  return(Object.assign(object, {key: value}))
+  return(Object.assign(object, {[key]: value}))
 }
